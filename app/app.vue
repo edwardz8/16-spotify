16bc410f546ed9b16b7c34bdd192a489d0308fde
Zach Edwards <tolklorist@gmail.com>
2017-02-28 20:42:37 -0600

    css works

---
+++ app/app.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="app">
+    <div class="page">
+      <div class="panel">
+        <div class="panel-block">
+          <div class="results">
+            <ul class="track">
+              <p class="track__media"></p>
+              <img src="#" alt="" class="track__frame">
+              <h5 class="track__artist"></h5>
+              <p class="track__name"></p>
+            </ul>
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
